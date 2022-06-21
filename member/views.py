068f59bbdb88aae17894bd17c01f7466166f1a0b
midorimame <sykwon97@naver.com>
2022-06-21 12:38:38 +0900

    Django3 urls.py, views.py 생성 - home, member, board

--- member/views.py
+++ member/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+    return render(request,'member/join.html')
+
+def login(request):
+    return render(request,'member/login.html')
+
+def member(request):
+    return render(request,'member/myinfo.html')
