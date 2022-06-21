b2a51fe10eadffdd09f37fd85c8248f2856c6184
midorimame <sykwon97@naver.com>
2022-06-21 13:12:56 +0900

    Django3 views.py 파일 수정

--- member/views.py
+++ member/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-def list(request):
+def join(request):
     return render(request,'member/join.html')
 
 def login(request):
     return render(request,'member/login.html')
 
-def member(request):
+def myinfo(request):
     return render(request,'member/myinfo.html')
+
