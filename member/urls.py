da337d572c941173b890d41d8d12f05739bdae61
midorimame <sykwon97@naver.com>
2022-06-23 12:04:36 +0900

    Django3 member join views, template 수정-클라이언트측/서버측 유효성 검사, Member 객체 생성 후 DB저장, 가입 완료시 ok 페이지로 전환

--- member/urls.py
+++ member/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1/join 요청 시, member apps의 join함수가 처리함
     path('join/', views.join, name='join'),
     path('login/', views.login, name='login'),
     path('myinfo/', views.myinfo, name='myinfo'),
